f6cbbca5c9629df96c5b26b03779d01d419eca99
Yedarm Seong <mybirth0407@gmail.com>
2023-12-26 23:50:09 +0900

    update

--- linux/do_not_run.py
+++
@@ -1,5 +0,0 @@
-import time
-
-while True:
-    print('Hello, Welcome to Garudate School!')
-    time.sleep(5)
--- linux/square.py
+++
@@ -1,5 +0,0 @@
-for i in range(10):
-    j = input('input number: ')
-    print(j)
-    j = int(j)
-    print(f'output: {j*j}')
